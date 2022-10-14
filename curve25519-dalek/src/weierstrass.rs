d7a9770d3bcaf43b65d75216a2569792aa0fa3df
schradeyannik <developer@yannik-schrade.de>
2022-10-14 17:36:34 +0200

    Standard short-Weierstrass point addition added

--- curve25519-dalek/src/weierstrass.rs
+++ curve25519-dalek/src/weierstrass.rs
@@ -1,4 +1,4 @@
-use core::ops::BitAndAssign;
+use core::ops::{Add, BitAndAssign};
 
 use field::FieldElement;
 use subtle::Choice;
@@ -39,6 +39,32 @@ impl ConditionallySelectable for WeierstrassPoint {
                 &FieldElement::from_bytes(&b.x),
                 choice,
             ).to_bytes(),
+        }
+    }
+}
+
+impl Add for WeierstrassPoint {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        // Non-jacobian short-Weierstrass affine addition (https://www.hyperelliptic.org/EFD/g1p/auto-shortw.html)
+        // u = (y2-y1)/(x2-x1)
+        // x3 = u^2-x1-x2
+        // y3 = u*(x1-x3)-y1
+
+        let x1 = FieldElement::from_bytes(&self.x);
+        let y1 = FieldElement::from_bytes(&self.y);
+
+        let x2 = FieldElement::from_bytes(&rhs.x);
+        let y2 = FieldElement::from_bytes(&rhs.y);
+
+        let u = &(&y2 - &y1) * &(&x2 - &x1).invert();
+        let x3 = &(&u.square() - &x1) - &x2;
+        let y3 = &(&u * &(&x1 - &x3)) - &y1;
+
+        WeierstrassPoint {
+            x: x3.to_bytes(),
+            y: y3.to_bytes(),
         }
     }
 }
